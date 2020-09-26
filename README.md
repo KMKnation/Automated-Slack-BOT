d331af33bc2eaf7d1dbdfefbfa74044815edc766
mayur.kanojiya <mayur.kanojiya@hiddenbrains.in>
2020-09-26 17:02:10 +0530

    docs: removed unused image from readme

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Automated-Slack-BOT
 
 #### Demo:
+The Bot uses the account of @Mayur Kanojiya (see screenshots ) 
 
 ##### 1. 
 ![BOT](images/screenshot_from_2020-09-26_10-29-10.png)
--- Test Cases/Spam1.java
+++
@@ -1,8 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<TestCaseEntity>
-   <description></description>
-   <name>Spam1</name>
-   <tag></tag>
-   <comment></comment>
-   <testCaseGuid>16029f16-27a2-4ea3-93f8-da1001c0ef11</testCaseGuid>
-</TestCaseEntity>
